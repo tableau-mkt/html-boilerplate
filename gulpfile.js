06525a20a6489c545d40fcd4c4c3bb73baaea192
BJ Miller <bjmiller@tableau.com>
2018-05-07 19:30:27 -0700

    Initial commit of all work

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var twig = require('gulp-twig');
+var yamlData = require('gulp-yaml-data');
+var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
+
+gulp.task('compile', function() {
+  return gulp.src('./src/templates/index.twig')
+    .pipe(yamlData({
+      property: 'data',
+      src: './src/data/examples.yml'
+    }))
+    .pipe(twig())
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch(['./src/data/**/*.json', './src/data/**/*.yml', './src/templates/**/*.twig'], ['compile']);
+});
+
+gulp.task('default', ['compile', 'watch']);
